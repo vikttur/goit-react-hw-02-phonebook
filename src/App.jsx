30d8c992223cc31fc3ac91119bb7d2e213b9961a
Viktor Biruk <vikttur.job@gmail.com>
2023-06-14 23:18:12 +0300

    checks for a new contact in the list

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import Filter from './components/Filter/Filter';
 import ContactList from './components/ContactList/ContactList';
 import Notification from './components/Notification/Notification';
 
-const TEST_CONTACTS = [
+const LIST_CONTACTS = [
   { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
   { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
   { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
@@ -15,8 +15,7 @@ const TEST_CONTACTS = [
 
 export default class App extends Component {
   state = {
-    contacts: TEST_CONTACTS,
-    // contacts: [],
+    contacts: LIST_CONTACTS,
     filter: '',
   };
 
@@ -31,9 +30,23 @@ export default class App extends Component {
   };
 
   addContact = contact => {
+    const { contacts } = this.state;
+    const { name } = contact;
+
+    if (this.isExist(contacts, name)) {
+      alert(`${name} is already in contacts.`);
+      return;
+    }
+
     this.setState(({ contacts }) => ({
       contacts: [contact, ...contacts],
     }));
+  };
+
+  isExist = (contacts, name) => {
+    return contacts.find(
+      contact => contact.name.toLowerCase() === name.toLowerCase()
+    );
   };
 
   handlerFilterChange = e => {
--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -47,8 +47,8 @@ export default class ContactForm extends Component {
             value={name}
             placeholder="Enter name"
             // pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-            title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-            required
+            // title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+            // required
             onChange={this.handleInputChange}
           />
         </label>
@@ -63,8 +63,8 @@ export default class ContactForm extends Component {
             value={number}
             placeholder="Enter the phone number"
             // pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
-            title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
-            required
+            // title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+            // required
             onChange={this.handleInputChange}
           />
         </label>
